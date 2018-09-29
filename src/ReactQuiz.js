901ea1057ac56b64f8645e919720733dfd41de4a
Micah Caffey <mrcaffey@gmail.com>
2018-09-29 17:55:04 -0600

    rendered React Quiz styling to page, rearranged JS files to reflect name

---
+++ src/ReactQuiz.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './index.css';
+import './bootstrap.min.css';
+
+class ReactQuiz extends Component {
+  render() {
+    return (
+      <div> React Quiz! </div>
+    );
+  }
+}
+
+export default ReactQuiz;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ReactQuiz from './ReactQuiz';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ReactQuiz />, document.getElementById('root'));
 registerServiceWorker();
