58f82b48047a840e8675c71ecb7c093230b58dae
Micah Caffey <mrcaffey@gmail.com>
2018-09-29 22:39:05 -0600

    added authors to authors array, added yarn underscore functionality

--- src/ReactQuiz.js
+++ src/ReactQuiz.js
@@ -25,7 +25,7 @@ function Turn({author, books}) {
       <img src={author.imageUrl} className="authorimage" alt="Author"/>
     </div>
     <div className="col-6">
-      {books.map((title) => <Book title={title} key={title} )}
+      {books.map((title) => <Book title={title} key={title}></Book> )}
     </div>
   </div>);
 }
--- src/index.js
+++ src/index.js
@@ -3,25 +3,47 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import ReactQuiz from './ReactQuiz';
 import registerServiceWorker from './registerServiceWorker';
+import {shuffle} from 'underscore';
 
 
 const authors = [
   {
     name: 'David Benioff',
-    imageUrl: 'images/authors/davidbenioff.jpeg',
+    imageUrl: 'images/authors/davidbenioff.jpg',
     imageSource: 'Wikipedia',
     books: ['City of Thieves',
             'The 25th Hour',
             'When the Nines Roll Over'
           ]
+  },
+  {
+    name: 'Leo Tolstoy',
+    imageUrl: 'images/authors/tolstoy.jpg',
+    imageSource: 'Google',
+    books: ['War and Peace',
+            'The Death of Ivan Ilyich',
+            'Anna Karenina'
+  ]
+},
+{
+    name: 'Isaac Asimov',
+    imageUrl: 'images/authors/asimov_isaac.jpg',
+    imageSource: 'The Olympians',
+    books: [ 'The Foundation Trilogy',
+             'The Stars, Like Dust',
+             'Fantasic Voyage'
+]
   }
 ];
 
+function getTurnData(authors) {
+  const allBooks = authors.reduce(function (p,c, i) {
+    return p.concat(c.books);
+  }, []);
+}
+
 const state =  {
-  turnData: {
-    author: authors[0],
-    books: authors[0].books
-  }
+  turnData: getTurnData(authors)
 };
 
 ReactDOM.render(<ReactQuiz {...state} />, document.getElementById('root'));
