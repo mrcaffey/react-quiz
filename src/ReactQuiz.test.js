77fb322726e083e16e609e914000e560168f3f32
Micah Caffey <mrcaffey@gmail.com>
2018-09-29 20:19:36 -0600

    added simple application testing method

--- src/ReactQuiz.test.js
+++ src/ReactQuiz.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import ReactQuiz from './ReactQuiz';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+
+describe("React Quiz", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<ReactQuiz />, div);
+  });
 });
