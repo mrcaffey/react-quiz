8572f23369adc2328bc8ce2cccc6996ab845b967
Micah Caffey <mrcaffey@gmail.com>
2018-09-30 19:09:08 -0600

    finished up react.test.js

--- src/ReactQuiz.test.js
+++ src/ReactQuiz.test.js
@@ -24,10 +24,46 @@ describe("React Quiz", () => {
     ReactDOM.render(<ReactQuiz {...state} onAnswerSelected={()=>{ }} />, div);
   });
 
-  describe("When no answer has been selected", ()=>{
+  describe("When the correct answer has been", ()=>{
     let wrapper;
     beforeAll(()=> {
     wrapper = mount(<ReactQuiz {...state} onAnswerSelected={()=> {}}/>);
+    });
+
+    it("should have no background color", () => {
+      expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe('fuschia');
+  });
+
+
+  describe('When the wrong answer has been selected', () => {
+    let wrapper;
+
+    beforeAll(() => {
+      wrapper = mount(
+      <AuthorQuiz {...(Object.assign({}, state, {highlight: 'wrong'}))} onAnswerSelected={()=>{ }} />, div);
+ });
+
+    it('should have a red background color', () => {
+      expect(wrapper.find('div.row.turn').props().style.backgroundColor).toBe('red');
+  });
+});
+
+    describe("When the first answer is selected")
+      let wrapper;
+      const handleAnswerSelected = jest.fn();
+
+      beforeAll(()=>{
+        wrapper = mount(
+        <ReactQuiz {...state} onAnswerSelected={handleAnswerSelected} />);
+      wrapper.find('.answer').first().simulate('click');
+    });
+
+    it("onAnswerSelected should be called", ()=>{
+      expect(handleAnswerSelected).toHaveBeenCalled();
+    });
+
+    it("should receive The Shining", ()=>{
+      expect(handleAnswerSelected).toHaveBeenCalled("The Shining")
     });
   });
 });
